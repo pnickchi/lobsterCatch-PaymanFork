6f03e886332fcdc54e8a558d868109da0e2778d3
vpourfaraj <vpourfaraj@gmail.com>
2022-09-16 13:36:06 -0300

    comments added to package run

--- inst/extdata/packageRun.R
+++ inst/extdata/packageRun.R
@@ -19,9 +19,9 @@ p = list()
 p$nrowgrids = 10
 p$ncolgrids = 10
 p$ngrids = p$nrowgrids * p$ncolgrids
-p$initlambda = 0.2
-p$initD = 3
-p$shrinkage = 0.993
+p$initlambda = 0.2 #is the density of lobsters at the beginning of simulation
+p$initD = 3 #is the dispersion index of lobsters on seabed at the beginning of the simulation
+p$shrinkage = 0.993 # attraction to bait I(t) = Shrinkage * I(t-1)
 p$currentZoI = 15
 p$radiusOfInfluence = 15
 p$Trap = data.frame( x = c(3,5,6), y = c(3,5,6) )
@@ -30,7 +30,7 @@ p$saturationThreshold = 5
 p$howClose = 0.5
 p$dStep = 5
 p$lengthBased = TRUE
-p$lobsterSizeFile <- 'LobsterSizeFreqs.csv' 
+p$lobsterSizeFile <- 'LobsterSizeFreqs.csv'
 p$lobLengthThreshold = 115
 p$trapSaturation = TRUE
 p$q0 = 0.5
@@ -52,7 +52,7 @@ Results  <- GetSimOutput(TrialSim)
 
 ## Changing Shrinkage factor
 # Folder: ImpaceofShrinkageOnMean
-# This folder saves all simulated data, parameters, and plots to compare mean of catch at 
+# This folder saves all simulated data, parameters, and plots to compare mean of catch at
 # each shrinkage parameter.
 ##
 
@@ -60,24 +60,24 @@ shrinkage = seq(0.95, 1, by =0.01)
 meanCatch = list()
 meanDispersion = list()
 for(j in 1:length(shrinkage)){
-  
+
   print(shrinkage[j])
   p$shrinkage = shrinkage[j]
-  
+
   TrialSim <- SimulateLobsterMovement(p)
   Results  <- GetSimOutput(TrialSim)
-  
-  
+
+
   meanCatch[[j]]      = mean( apply(X = Results$MaxCatch, MARGIN = 2, FUN = mean) )
-  
+
   if( ncol(Results$MaxCatch) == 1 ){
     meanDispersion[[j]] = mean( apply(X = Results$MaxCatch, MARGIN = 2, FUN = dispersion) )
   }
-  
+
   if( ncol(Results$MaxCatch) > 1 ){
     meanDispersion[[j]] = mean( apply(X = Results$MaxCatch, MARGIN = 1, FUN = dispersion) )
   }
-  
+
 }
 
 sim_number <- 2
@@ -89,24 +89,24 @@ if( !dir.exists( paste0('ImpactofShrinkageOnMean/sim', sim_number) ) ) {
 png(filename = paste0('ImpactofShrinkageOnMean/sim',sim_number,'/ImpaceofShrinkageOnMeanCatch.PNG'), width = 800, height = 600 )
 plot(x = shrinkage,
      y = meanCatch,
-     type = 'b', 
-     xlab = 'Shrinkage Factor', 
+     type = 'b',
+     xlab = 'Shrinkage Factor',
      ylab = 'Mean of Catch')
 dev.off()
 
 png(filename = paste0('ImpactofShrinkageOnMean/sim',sim_number,'/ImpactofShrinkageOnDisperssion.PNG'), width = 800, height = 600 )
 plot(x = shrinkage,
      y = meanDispersion,
-     type = 'b', 
-     xlab = 'Shrinkage Factor', 
+     type = 'b',
+     xlab = 'Shrinkage Factor',
      ylab = 'Mean of Disperssion')
 dev.off()
 
 
-save(meanCatch, 
-     meanDispersion, 
-     p, 
-     shrinkage, 
+save(meanCatch,
+     meanDispersion,
+     p,
+     shrinkage,
      file = paste0('ImpactofShrinkageOnMean/Sim',sim_number,'/all.RData')
 )
 
@@ -119,7 +119,7 @@ save(meanCatch,
 
 ## Changing Lambda
 # Folder: ImpactofDensityOnMean
-# This folder saves all simulated data, parameters, and plots to compare mean of catch at 
+# This folder saves all simulated data, parameters, and plots to compare mean of catch at
 # each lambda parameter.
 ##
 
@@ -127,48 +127,48 @@ lambda = c(0.1, 0.5, 1, 1.6)
 meanCatch = list()
 meanDispersion = list()
 for(j in 1:length(lambda)){
-  
+
   print(lambda[j])
   p$initlambda = lambda[j]
-  
+
   TrialSim <- SimulateLobsterMovement(p)
   Results  <- GetSimOutput(TrialSim)
-  
-  
+
+
   meanCatch[[j]]      = mean( apply(X = Results$MaxCatch, MARGIN = 2, FUN = mean) )
-  
+
   if( ncol(Results$MaxCatch) == 1 ){
     meanDispersion[[j]] = mean( apply(X = Results$MaxCatch, MARGIN = 2, FUN = dispersion) )
   }
-  
+
   if( ncol(Results$MaxCatch) > 1 ){
     meanDispersion[[j]] = mean( apply(X = Results$MaxCatch, MARGIN = 1, FUN = dispersion) )
   }
-  
+
 }
 
 sim_number <- 1
 png(filename = paste0('ImpactofDensityOnMean/sim',sim_number,'/ImpactofDensityOnMeanCatch.PNG'), width = 800, height = 600 )
 plot(x = shrinkage,
      y = meanCatch,
-     type = 'b', 
-     xlab = 'Lambda', 
+     type = 'b',
+     xlab = 'Lambda',
      ylab = 'Mean of Catch')
 dev.off()
 
 png(filename = paste0('ImpactofDensityOnMean/sim',sim_number,'/ImpactofDensityOnDisperssion.PNG'), width = 800, height = 600 )
 plot(x = shrinkage,
      y = meanDispersion,
-     type = 'b', 
-     xlab = 'Lambda', 
+     type = 'b',
+     xlab = 'Lambda',
      ylab = 'Mean of Disperssion')
 dev.off()
 
 
-save(meanCatch, 
-     meanDispersion, 
-     p, 
-     shrinkage, 
+save(meanCatch,
+     meanDispersion,
+     p,
+     shrinkage,
      file = paste0('ImpactofDensityOnMean/Sim',sim_number,'/all.RData')
 )
 

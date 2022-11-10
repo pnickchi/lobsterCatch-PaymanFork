07ee1bdefd8cb32f2abf3a56c4ca123dc498cc6d
vpourfaraj <vpourfaraj@gmail.com>
2022-11-10 10:27:20 -0400

    no major change

--- R/GetSimOutput.R
+++ R/GetSimOutput.R
@@ -1,11 +1,11 @@
-#' This function extracts two pieces of information at the end of simulation: maximum numbr of trapped
-#' lobster and the time when that occurred. 
+#' This function extracts two pieces of information at the end of simulation: maximum number of trapped
+#' lobster and the time when that occurred.
 #' @param x is an object that contains information from function SimulateLobsterMovement
 #' @param mls is the minimum legal size for calculating catch weight
 #' @return Returns  maximum catch in number total weight, and legal wt and length of time to reach maximum catch
 #' @export
 GetSimOutput = function(x,mls=82.5){
-  
+
   time.to.max <- list()
   max.catch   <- list()
   legwts      <- list()
@@ -13,7 +13,7 @@ GetSimOutput = function(x,mls=82.5){
   for( i in 1:length(x) ){
     time.to.max[[i]] = apply(x[[i]]$traps, 2, which.max)
     max.catch[[i]]   = apply(x[[i]]$traps, 2, max)
-  
+
         legwtpertrap =  c()
         wtpertrap = c()
           s = x[[i]]$lobSize
@@ -29,13 +29,13 @@ GetSimOutput = function(x,mls=82.5){
         legwts[[i]] = legwtpertrap
         totwts[[i]] = wtpertrap
   }
-  
-  
+
+
   time.to.max = as.data.frame(do.call(rbind,time.to.max))
   max.catch   = as.data.frame(do.call(rbind,max.catch))
   legwts      = as.data.frame(do.call(rbind,legwts))
   totwts      = as.data.frame(do.call(rbind,totwts))
-  
+
   names(legwts) = names(totwts) = names(time.to.max)
 
   return( list(TimeToMax = time.to.max, MaxCatch = max.catch, LegalCatchWt = legwts, TotalCatchWt = totwts ) )
--- R/SimulateLobsterMovement.R
+++ R/SimulateLobsterMovement.R
@@ -1,11 +1,11 @@
-#' A function to run the simulation based on defined parameters 
+#' A function to run the simulation based on defined parameters
 #' @param p is a list which contains all input variables
 #' @return Returns a list
 #' @export
 
 
 SimulateLobsterMovement = function(p){
-  #commented lines below: To be used for debugging purposes only 
+  #commented lines below: To be used for debugging purposes only
   # nrowgrids  <- p$nrowgrids
   # ncolgrids  <- p$ncolgrids
   # initlambda <- p$initlambda
@@ -27,79 +27,79 @@ SimulateLobsterMovement = function(p){
   # radiusOfInfluence <- p$radiusOfInfluence
   # lobsterSexDist <- p$lobsterSexDist
   # lobsterSizeFile <- p$lobsterSizeFile
-  
+
   with(p, {
-    
+
   if( (p$lengthBased == TRUE) & (p$lobsterSizeFile == '') ){
       lobsterSizeFile   <- file.choose()
       p$lobsterSizeFile <- lobsterSizeFile
   }
-    
+
   CatchSimulationOutput = list()
   for(k in 1:p$realizations){
-    
+
     start     <- Sys.time()
     outputs   <- list()
     outputs$traps    = rep(0, times = ntraps)
     outputs$lobsters = data.frame(EASTING = 0, NORTHING = 0, trapped=0, T = 0, I = 0, lobLength = 0)
-    
-    
+
+
     coordinatesOverTime      <- list()
     coordinatesOverTime[[1]] <- initialLobsterGrid(nrowgrids, ncolgrids, initlambda, initD, lobsterSizeFile, lobsterSexDist)
-    
-    
+
+
     trapCatch           <- list()
-    lobSize             <- list() 
-    lobSex              <- list() 
+    lobSize             <- list()
+    lobSex              <- list()
     trapCatch[[1]]      <- rep(0, length=ntraps)
     lobSize[[1]]        <- rep('',length=ntraps)
     lobSex[[1]]         <- rep('',length=ntraps)
-    
-      
+
+
     for(t in 2:tSteps){
-      
+
       if(t>2){currentZoI<- currentZoI * shrinkage}
-        
-      tempUpdateGrid = updateGrid(Lobster = coordinatesOverTime[[t-1]], 
-                                  Trap = Trap, 
-                                  trapCatch = trapCatch[[t-1]], 
+
+      tempUpdateGrid = updateGrid(Lobster = coordinatesOverTime[[t-1]],
+                                  Trap = Trap,
+                                  trapCatch = trapCatch[[t-1]],
                                   lobSize = lobSize[[t-1]],
                                   lobSex  = lobSex[[t-1]],
                                   radiusOfInfluence = radiusOfInfluence,
-                                  currentZoI = currentZoI, dStep = dStep, howClose = howClose, q0 = q0, qmin = qmin, 
+                                  currentZoI = currentZoI, dStep = dStep, howClose = howClose, q0 = q0, qmin = qmin,
                                   saturationThreshold = saturationThreshold, trapSaturation = trapSaturation, lengthBased = lengthBased,
                                   lobLengthThreshold = lobLengthThreshold, sexBased = sexBased)
       # Adam: DO you think, this would help?
-      #In case we ran out of space (memory)  the following line can be moved out of 
-      #the loop so that we simply just Just keep the last iteration? To make things faster 
+      #the following line can be moved out of
+      #the loop so that we simply just Just keep the last iteration? To make things faster
       #and less memory demanding?
       coordinatesOverTime[[t]] <- tempUpdateGrid[[1]]
       trapCatch[[t]]           <- tempUpdateGrid[[2]]
       lobSize[[t]]             <- tempUpdateGrid[[3]]
       lobSex[[t]]              <- tempUpdateGrid[[4]]
     }
-    
-    
+
+
     outmove   = do.call(rbind, coordinatesOverTime)
     outmove$TimeStep = rep(0:(tSteps-1), each = nrow(coordinatesOverTime[[1]]) )
     outmove$LobIndx = rep(1:nrow(coordinatesOverTime[[1]]), times=tSteps)
-    
+
     outtraps   = as.data.frame(do.call(rbind, trapCatch))
     outlobsize = as.data.frame(do.call(rbind, lobSize)  )
     outlobsex  = as.data.frame(do.call(rbind, lobSex)   )
     colnames(outtraps)   = paste0( 'Trap', 1:ncol(outtraps) )
     colnames(outlobsize) = paste0( 'Trap', 1:ncol(outtraps) )
     colnames(outlobsex)  = paste0( 'Trap', 1:ncol(outtraps) )
-    
+
     outputs$traps    = outtraps
     outputs$lobsters = outmove
-    outputs$lobSize  = outlobsize 
-    outputs$lobSex   = outlobsex 
-    
+    outputs$lobSize  = outlobsize
+    outputs$lobSex   = outlobsex
+
     CatchSimulationOutput[[k]] = outputs
     print(paste('Timing', Sys.time()-start, 'for iteration #',k,sep=" "))
   }
   return(CatchSimulationOutput)
   })
-  
+
 }
